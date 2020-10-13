5e6a180c1d2cef4923e20d223a480b16504a4baf
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-13 17:33:16 +0800

    [Zhangjin]: feat: implentments addStudent button

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ $(function() {
             });
     });
 
+    
+
     //alert();
     //getData("http://localhost:8080/student/list").then((res)=>console.log(res));
     let url="http://localhost:8080/student/list?isShuffle=0";
@@ -33,8 +35,12 @@ $(function() {
     .then(result => {
         console.log(result);
         for(let i=0;i<result.length;i++){
-            $(".stu-list").append("<li>"+result[i].id+result[i].name+"</li>")
+            $(".stu-list").append("<li>"+result[i].id+" "+result[i].name+"</li>")
         }
+        $(".stu-list").append("<li>+添加学员</li>");
+        $(".stu-list").append('<input type="text" placeholder="请输入学生姓名" style="display:none" id="stuName" />')
+        $(".stu-list").append('<button type="button" id="addStuBtn" style="display:none" >提交</button>');
+        bindClick();
     })
     .catch(error => {
         console.error(error);
@@ -64,13 +70,38 @@ function parseGroupList(result){
         <ul class=\"groupstu-list\">";
         for(let k=0;k<num+1;k++){
             if(k*6+j<total){
-                beginStr+=("<li>"+result[k*6+j].id+result[k*6+j].name+"</li>");
+                beginStr+=("<li>"+result[k*6+j].id+" "+result[k*6+j].name+"</li>");
             }
         }
         let endstr="</ul></div>";
         $(".group-list").append(beginStr+endstr);
     }
 
+}
+
+function bindClick(){
+    $(".stu-list li:last").click(function(){
+        $("#stuName").show();
+        $("#addStuBtn").show();
+    })
+
+    $("#addStuBtn").click(function(){
+    //    alert();
+        let name=$("#stuName").val();
+        let url="http://localhost:8080/student/list";
+        let json="{\"name\":\""+name+"\"}";
+        fetch(url,{
+            method:"POST",
+            headers: {"Content-Type":"application/json",'Access-Control-Allow-Origin':'*'},
+            body:json
+        }).then((response)=>{
+            if(response.status==200){
+                alert("添加成功！");
+            }else{
+                alert("添加失败！错误码："+response.status);
+            }
+        })
+    })
 }
 
 
